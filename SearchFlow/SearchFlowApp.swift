a1b3b7bb9a8b7678b1d7035e3dee50018e4c1648
david <davidzhao@mail.com>
2020-12-10 14:18:13 +0800

    Initial Commit

---
+++ SearchFlow/SearchFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchFlowApp.swift
+//  SearchFlow
+//
+//  Created by David on 12/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SearchFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
