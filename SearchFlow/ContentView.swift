ab578bec229d09606f62d39759e43916a69fb2d3
david <davidzhao@mail.com>
2020-12-13 16:19:52 +0800

    fetchlocal and fetchonline

--- SearchFlow/ContentView.swift
+++ SearchFlow/ContentView.swift
@@ -6,77 +6,116 @@
 //
 
 import SwiftUI
+import Combine
+
+let inputBackgroundColor = Color(.sRGB, red: 235/255, green: 235/255, blue: 235/255, opacity: 1)
+let listBackgroundColor = Color(.sRGB, red: 247/255, green: 247/255, blue: 247/255, opacity: 1)
+
+
+class ResultViewModel: ObservableObject {
+    
+    private let networkService = ProductService()
+    
+    @Published var textToSearch = ""
+    @Published var groupProductViewModels = [GroupProductViewModel]()
+     
+    var lastSearch: String = ""
+    
+    var cancellable: AnyCancellable?
+
+    func fetchDataLocal() {
+        guard textToSearch.replacingOccurrences(of: " ", with: "") != "" else {
+            return
+        }
+        guard self.textToSearch != lastSearch else {
+            return
+        }
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.groupProductViewModels = (self?.networkService.fetchLocal(text: self?.textToSearch).data.map { GroupProductViewModel($0) }) ?? []
+            self?.lastSearch = self?.textToSearch ?? ""
+        }
+    }
+    
+    func fetchDataOnline() {
+        guard textToSearch.replacingOccurrences(of: " ", with: "") != "" else {
+            return
+        }
+        guard self.textToSearch != lastSearch else {
+            return
+        }
+        cancellable = networkService.fetch(text: textToSearch).sink(receiveCompletion: { (rc) in
+            print("receive completion")
+            print(rc)
+        }, receiveValue: { (dc) in
+            self.groupProductViewModels = dc.data.map { GroupProductViewModel($0) }
+            print(self.groupProductViewModels)
+            self.lastSearch = self.textToSearch
+        })
+    }
+
+}
 
 struct ContentView: View {
-    
-    @State var textToSearch = ""
-    @State var isSearching = false
+
+    @ObservedObject private var viewModel = ResultViewModel()
     
     var body: some View {
         NavigationView {
             ScrollView {
-                
-                SearchBar(textToSearch: $textToSearch, isSearching: $isSearching)
-                
-                ForEach (requestDataWith(text: textToSearch)) { oneGroup in
-                    Section(header:
-                                Text(oneGroup.type.rawValue)
-                                    .frame(width: 400, height: 40, alignment: .leading)
-                                    .font(.title)
-                                    .foregroundColor(Color(.systemGray))) {
-                                Spacer()
-                        ForEach(oneGroup.products) { product in
-                            HStack {
-                                VStack (alignment: .leading) {
-                                    Text("\(product.name)")
-                                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                                    Spacer()
-                                    Text(product.inStock ? "in-stock" : "out-of-stock" )
-                                        .font(.body)
-                                        .foregroundColor(Color(.systemGray))
-                                }
-                                Spacer()
-                                Text("$\(product.price.description)")
-                                    .padding(.vertical, 4)
-                                    .padding(.horizontal, 10)
-                                    .background(Color(.systemGray6))
-                                    .cornerRadius(8)
-                                    .foregroundColor(product.inStock ? Color(.systemBlue) : Color(.systemGray))
-                            }
-                            .padding()
-                            Divider()
-                                .background(Color(.systemGray6))
-                                .padding(.leading)
-                        }
-                    }
+                HStack {
+                    SearchBar(resultViewModel: viewModel)
+                    .padding()
                 }
-                if textToSearch != "" && requestDataWith(text: textToSearch).count == 0 {
+                ForEach (viewModel.groupProductViewModels) { groupViewModel in
+                    ProductGroupView(groupViewModel: groupViewModel)
+                }
+
+                if viewModel.textToSearch != "" && viewModel.groupProductViewModels.count == 0 {
                     Text("No result")
                         .foregroundColor(Color(.systemGray))
                         .padding(.top, 40)
                 }
             }
+            .background(listBackgroundColor)
             .navigationTitle("Search")
         }
     }
+    
 }
 
-extension ContentView: DataAPI {}
+
 
 struct SearchBar: View {
     
-    @Binding var textToSearch: String
-    @Binding var isSearching: Bool
+    @ObservedObject var resultViewModel: ResultViewModel
+    @State var isSearching = false
     
     var body: some View {
         HStack {
-            TextField("Tap here to search", text: $textToSearch)
-                .padding(.leading, 25)
+            TextField("Tap here to search", text: $resultViewModel.textToSearch, onEditingChanged: { (value) in
+                
+            }, onCommit: {
+                // keyboard tap 'return'
+                if resultViewModel.textToSearch.replacingOccurrences(of: " ", with: "") == "" {
+                    DispatchQueue.main.async {
+                        isSearching = false
+                    }
+//                    isSearching = false
+                }
+            })
+            .onReceive(resultViewModel.textToSearch.publisher.collect()) {
+                // textfield text change to fetch
+                _ = $0.map(String.init).joined()
+                resultViewModel.fetchDataLocal()
+//                resultViewModel.fetchDataOnline()
+            }
+            // can also use 'publisher.reduce' to get text
+                .padding(.leading, 35)
         }
-        .padding()
-        .background(Color(.systemGray6))
-        .cornerRadius(5)
-        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .background(inputBackgroundColor)
+        .cornerRadius(10)
         .onTapGesture(perform: {
             isSearching = true
         })
@@ -85,15 +124,146 @@ struct SearchBar: View {
                 Image(systemName: "magnifyingglass")
                 Spacer()
                 if isSearching == true {
-                    Button(action: { textToSearch = ""}, label: {
+                    Button(action: {
+                        DispatchQueue.main.async {
+                            resultViewModel.textToSearch = ""
+                            resultViewModel.groupProductViewModels = []
+                        }
+                    }, label: {
                         Image(systemName: "xmark")
                             .padding(.vertical)
                     })
                 }
             }
-            .padding(.horizontal, 30)
+            .padding(.horizontal, 10)
             .foregroundColor(Color(.systemGray2))
         )
+    }
+}
+
+// GroupProducView
+struct GroupProductViewModel: Identifiable {
+    
+    private let oneGroup: GroupedProduct
+    
+    var id: String {
+        return oneGroup.id
+    }
+
+    var type: ProductType {
+        return oneGroup.type
+    }
+    
+    var productViewModels: [ProductViewModel] {
+        return oneGroup.products.map { ProductViewModel(oneProduct: $0) }
+    }
+    
+    init(_ oneGroup: GroupedProduct) {
+        self.oneGroup = oneGroup
+    }
+}
+
+struct ProductGroupView: View {
+    var groupViewModel: GroupProductViewModel
+    var body: some View {
+        Section(header:
+                    Text(groupViewModel.type.rawValue)
+                        .frame(width: 400, height: 30, alignment: .leading)
+                        .font(.title3)
+                        .background(listBackgroundColor)
+                        .foregroundColor(Color(.systemGray))) {
+                    Spacer()
+            ForEach(groupViewModel.productViewModels) { productViewModel in
+                ProductCellView(viewModel: productViewModel)
+            }
+        }
+    }
+}
+
+// ProductCellView
+struct ProductViewModel: Identifiable {
+    private let oneProduct: Product
+    init(oneProduct: Product) {
+        self.oneProduct = oneProduct
+    }
+
+    var id: String {
+        return oneProduct.id
+    }
+
+    var brand: String {
+        return oneProduct.brand
+    }
+
+    var name: String {
+        return oneProduct.name
+    }
+
+    var type: ProductType {
+        return oneProduct.type
+    }
+
+    var inStock: Bool {
+        return oneProduct.inStock
+    }
+
+    var formattedPrice: String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+
+        guard let price = Double(oneProduct.price), let formattedPrice = numberFormatter.string(from: NSNumber(value: price)) else {
+            return ""
+        }
+        return formattedPrice
+    }
+    
+    var priceColor: Color {
+        return oneProduct.inStock ? Color(.systemBlue) : Color(.systemGray)
+    }
+}
+
+struct ProductCellView: View {
+    var viewModel: ProductViewModel
+    
+    var body: some View {
+        VStack {
+            Spacer(minLength: 15)
+            HStack {
+                VStack (alignment: .leading) {
+                    Text("\(viewModel.name)")
+                        .font(.title2)
+                    Spacer()
+                    Text(viewModel.inStock ? "in-stock" : "out-of-stock" )
+                        .font(.body)
+                        .foregroundColor(Color(.systemGray))
+                }
+                Spacer()
+                Text(viewModel.formattedPrice)
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 10)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .foregroundColor(viewModel.priceColor)
+            }
+            .padding(.horizontal)
+            .contentShape(Rectangle())
+            .onTapGesture {
+                print("did select cell")
+            }
+            Spacer(minLength: 15)
+            Divider()
+        }
+        .background(Color.white)
+
+    }
+}
+
+struct CellDiveder: View {
+    var body: some View {
+        GeometryReader { gor in
+            Rectangle().fill(listBackgroundColor)
+                .frame(width: gor.size.width, height: 1)
+        }
     }
 }
 
---
+++ SearchFlow/ProductService.swift
@@ -0,0 +1,108 @@
+//
+//  NetworkService.swift
+//  SearchFlow
+//
+//  Created by David on 12/11/20.
+//
+
+import Foundation
+import Combine
+
+final class ProductService {
+    
+    var components: URLComponents {
+        var components = URLComponents()
+        components.scheme = "https"
+//        components.host = "10.0.1.147"
+        components.host = "sidt.ai"
+//        components.port = 8976
+        components.port = 8602
+//        components.path = "/searchflow/"
+        components.path = "/test/searchflow"
+
+        return components
+    }
+    
+    func request(text: String) -> URLRequest {
+        var request = URLRequest(url: components.url!, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 5)
+        request.httpMethod = "POST"
+        let dict = ["searchText": text]
+        let d = try! JSONSerialization.data(withJSONObject: dict, options: .fragmentsAllowed)
+        request.httpBody = d
+        return request
+    }
+    
+    func fetch(text: String) -> AnyPublisher<DataContainer, Error> {
+        return URLSession.shared.dataTaskPublisher(for: request(text: text))
+            .map{ $0.data }
+            .decode(type: DataContainer.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    func fetchLocal(text: String?) -> DataContainer {
+        guard let t = text, t.lowercased() == "dyson" else { return DataContainer(status: "succed", data: [])}
+        let str = "{\"status\": \"succed\", \"data\": [{\"ID\": \"234234\", \"productType\": \"Vacuum\", \"products\": [{\"ID\": \"489458\", \"brand\": \"Dyson\", \"name\": \"V11\", \"productType\": \"Vacuum\", \"inStock\": true, \"price\": \"599.99\"}, {\"ID\": \"334567\", \"brand\": \"Dyson\", \"name\": \"V10\", \"productType\": \"Vacuum\", \"inStock\": false, \"price\": \"399.99\"}]}, {\"ID\": \"234233\", \"productType\": \"Hair Dryer\", \"products\": [{\"ID\": \"898765\", \"brand\": \"Dyson\", \"name\": \"Supersonic\", \"productType\": \"Hair Dryer\", \"inStock\": true, \"price\": \"399.99\"}]}]}"
+        let data = str.data(using: .utf8)!
+        let dc = try! JSONDecoder().decode(DataContainer.self, from: data)
+        return dc
+    }
+}
+
+struct DataContainer: Decodable {
+    let status: String
+    let data: [GroupedProduct]
+}
+
+enum ProductType: String, Codable{
+    case UN = "Unknow"
+    case VC = "Vacuum"
+    case HD = "Hair Dryer"
+}
+
+struct Product: Identifiable, Decodable {
+    var id: String
+    var brand: String
+    var name: String
+    var type: ProductType
+    var inStock: Bool
+    var price: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "ID"
+        case brand = "brand"
+        case name = "name"
+        case type = "productType"
+        case inStock = "inStock"
+        case price = "price"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decodeIfPresent(String.self, forKey: .id) ?? "0"
+        brand = try container.decodeIfPresent(String.self, forKey: .brand) ?? "unknow brand"
+        name = try container.decodeIfPresent(String.self, forKey: .name) ?? "unknow name"
+        type = try container.decodeIfPresent(ProductType.self, forKey: .type) ?? ProductType.UN
+        inStock = try container.decodeIfPresent(Bool.self, forKey: .inStock) ?? false
+        price = try container.decodeIfPresent(String.self, forKey: .price) ?? "0.00"
+    }
+}
+
+struct GroupedProduct: Identifiable, Decodable {
+    var id: String
+    var type: ProductType
+    var products: [Product]
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "ID"
+        case type = "productType"
+        case products = "products"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decodeIfPresent(String.self, forKey: .id) ?? "0"
+        type = try container.decodeIfPresent(ProductType.self, forKey: .type) ?? ProductType.UN
+        products = try container.decodeIfPresent([Product].self, forKey: .products) ?? []
+    }
+}
